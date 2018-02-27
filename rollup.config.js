de6b2507d12a02d1c23d2a36410495bb11831334
FAL <falworks.contact@gmail.com>
2018-02-28 00:03:41 +0900

    v0.4.1

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import cleanup from 'rollup-plugin-cleanup';
 
 const moduleName = 'p5ex';
 const umdName = 'p5ex';
-const version = '0.4.0';
+const version = '0.4.1';
 const year = '2018';
 const description =
 `* An extension of p5.js.`;
