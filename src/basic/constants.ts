89db464af956ffe44392203067582300eff7b086
FAL <falworks.contact@gmail.com>
2018-02-27 17:58:53 +0900

    Added some constants.

--- src/basic/constants.ts
+++ src/basic/constants.ts
@@ -9,4 +9,8 @@ export const EMPTY_FUNCTION = () => { };
 export const enum KeyCodes {
   A = 65,
   B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
+  SPACE = 32,
 }
+
+export const ONE_AND_HALF_PI = 1.5 * Math.PI;
+export const ROOT_THREE = 1.73205080757;
