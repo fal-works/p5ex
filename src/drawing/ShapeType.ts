a0fff3128a6d77b03644623e35a0554a6d507d4e
FAL <falworks.contact@gmail.com>
2018-02-27 22:14:34 +0900

    Refactored for tree shaking.

--- src/drawing/ShapeType.ts
+++ src/drawing/ShapeType.ts
@@ -15,25 +15,14 @@ const SIN60 = Math.sqrt(3) / 2;
 /**
  * Set of shape types.
  */
-export namespace ShapeTypes {
-  /**
-   * A p5ex.ShapeType instance representing a circle.
-   */
-  export const CIRCLE = new ShapeType(
+export const ShapeTypes: { [key: string]: ShapeType } = {
+  CIRCLE: new ShapeType(
     (renderer: p5, size: number) => { renderer.ellipse(0, 0, size, size); },
-  );
-
-  /**
-   * A p5ex.ShapeType instance representing a square.
-   */
-  export const SQUARE = new ShapeType(
+  ),
+  SQUARE: new ShapeType(
     (renderer: p5, size: number) => { renderer.rect(0, 0, size, size); },
-  );
-
-  /**
-   * A p5ex.ShapeType instance representing a regular triangle.
-   */
-  export const REGULAR_TRIANGLE = new ShapeType(
+  ),
+  REGULAR_TRIANGLE: new ShapeType(
     (renderer: p5, size: number) => {
       const radius = 0.5 * size;
       renderer.triangle(
@@ -45,12 +34,8 @@ export namespace ShapeTypes {
         +SIN60 * radius,
       );
     },
-  );
-
-  /**
-   * A p5ex.ShapeType instance representing an upward regular triangle.
-   */
-  export const REGULAR_TRIANGLE_UPWARD = new ShapeType((renderer: p5, size: number) => {
+  ),
+  REGULAR_TRIANGLE_UPWARD: new ShapeType((renderer: p5, size: number) => {
     const radius = 0.5 * size;
     renderer.triangle(
       0,
@@ -60,6 +45,6 @@ export namespace ShapeTypes {
       +SIN60 * radius,
       -COS60 * radius,
     );
-  });
-}
+  }),
+};
 // tslint:enable
